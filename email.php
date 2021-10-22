d331cb8b7de2a73223e0df0500bbff18b8ff749a
Pablo Pulisich <pablopuli94@gmail.com>
2021-10-21 21:32:58 -0300

    Template

--- email.php
+++ email.php
@@ -13,16 +13,16 @@ try {
     //Server settings
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'files.000webhost.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'prestamos_i.c@outlook.com';                     //SMTP username
-    $mail->Password   = '25Mayo66';                         //SMTP password
+    $mail->Username   = 'info@cotizacioneshoy.online';                     //SMTP 
+    $mail->Password   = 'Karluncho2020';                         //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-    //Recipients
-    $mail->setFrom( 'prestamos_i.c@outlook.com', 'Pablo'); // Hacer coincidir con el username. (preferentemente)
-    $mail->addAddress('pablopuli94@gmail.com', 'Pablo');     //Add a recipient
+    //Recipients 
+    $mail->setFrom( 'info@cotizacioneshoy.online', 'Cotizaciones Hoy'); // Hacer coincidir con el username. (preferentemente)
+    $mail->addAddress('tengoganasdedormir01@gmail.com', 'Facu');     //Add a recipient
     //$mail->addAddress('ellen@example.com');               //Name is optional
     //$mail->addReplyTo('info@example.com', 'Information');
     //$mail->addCC('');
@@ -38,6 +38,9 @@ try {
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Consulta de: '.$_POST['nombre'];
+   
+    
+
     $mail->Body    = 
     'Nombre: '.$_POST['nombre'].
     '<br>Apellido: '. $_POST['apellido']. 
